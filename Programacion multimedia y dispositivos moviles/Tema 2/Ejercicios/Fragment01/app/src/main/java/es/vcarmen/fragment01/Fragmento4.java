58e0a43f4394dfd11377081fa53feda99f8f7188
ElRorro <juravideos@gmail.com>
2017-10-04 14:07:39 +0200

    4/10/2017

---
+++ Programacion multimedia y dispositivos moviles/Tema 2/Ejercicios/Fragment01/app/src/main/java/es/vcarmen/fragment01/Fragmento4.java
@@ -0,0 +1,17 @@
+package es.vcarmen.fragment01;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by matinal on 04/10/2017.
+ */
+
+public class Fragmento4 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragmento4, container, false);}
+}
