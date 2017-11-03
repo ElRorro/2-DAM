c3f5fabe2e451d04af0b0ec3519d2c2d04483855
ElRorro <juravideos@gmail.com>
2017-11-03 09:18:38 +0100

    03/11/2017

---
+++ Programacion multimedia y dispositivos moviles/Material Design/Material02/app/src/main/java/es/vcarmen/material02/activity_material02.java
@@ -0,0 +1,13 @@
+package es.vcarmen.material02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_material02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material02);
+    }
+}
